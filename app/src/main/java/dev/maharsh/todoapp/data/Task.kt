6a56d81abbb3b9e814926b5a3b02eb3e53fdba17
Maharsh Patel <49917427+themaharshpatel@users.noreply.github.com>
2022-05-26 01:42:55 +0530

    Proguard optimised

--- app/src/main/java/dev/maharsh/todoapp/data/Task.kt
+++ app/src/main/java/dev/maharsh/todoapp/data/Task.kt
@@ -1,11 +1,13 @@
 package dev.maharsh.todoapp.data
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.text.DateFormat
 
+@Keep
 @Parcelize
 @Entity(tableName = "TaskTable")
 data class Task(
